d49efe57a2ce7dc6ad574066490e332f1e2627fa
Miles <milsman2@gmail.com>
2022-11-02 02:10:24 +0000

    Initial working docker image

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPageWithLayout } from './_app'
+import Layout from '../components/Layout'
+
+const Index: NextPageWithLayout = () => {
+  return (
+    <section className='flex flex-col flex-1 bg-black text-white'>
+      <h2>Kane Bros. Lab</h2>
+      <p>This is just a placeholder while I learn some Typescript and Next.js</p>
+      <p>More coming soon!</p>
+    </section>
+  )
+}
+
+export default Index
+
+Index.getLayout = function getLayout(page: React.ReactElement) {
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  )
+}

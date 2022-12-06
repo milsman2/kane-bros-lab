babe874459298b8d8f721a7fadc0459fbfc46293
Miles <milsman2@gmail.com>
2022-12-06 02:06:25 +0000

    Feature testing.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
-import Head from 'next/head';
-import PersonPage from '../pages/person'
+import { useState } from 'react'
 import type { NextPageWithLayout } from './_app'
 import Layout from '../components/Layout'
+import Counter from '../components/Counter'
+import List from '../components/List'
 
 const Home: NextPageWithLayout = () => {
+  const [count, setCount] = useState<number>(1)
+
   return (
     <div className='bg-black text-white flex flex-col flex-1 items-center justify-center'>
-      <PersonPage />
+      <Counter setCount={setCount}>Count is {count}</Counter>
+      <List items={["Coffee", "Tacos", "Code"]} render={(item: string) => <span>{item}</span>} />
+      <div>More Next.js + TypeScript + React-Query coming soon.</div>
     </div>
   );
 };

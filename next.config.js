800adb9f4aa8ee29c55e05a9bb117f417ece956b
Miles <milsman2@gmail.com>
2022-12-23 22:44:19 +0000

    Fix lang tag.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone'
+  output: 'standalone',
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
 }
 
 module.exports = nextConfig

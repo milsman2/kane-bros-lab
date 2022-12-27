78037e28e43eb583a6be15164e5d1c2618d1dec3
Miles <milsman2@gmail.com>
2022-12-27 22:50:45 +0000

    Fix session provider.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,6 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
-import { SessionProvider } from 'next-auth/react';
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: React.ReactElement) => React.ReactNode;
@@ -37,12 +36,10 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   return getLayout(
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <SessionProvider session={pageProps.session}>
-          <ChakraProvider>
-            <ReactQueryDevtools initialIsOpen={false} />
-            <Component {...pageProps} />
-          </ChakraProvider>
-        </SessionProvider>
+        <ChakraProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <Component {...pageProps} />
+        </ChakraProvider>
       </Hydrate>
     </QueryClientProvider>
   );

f9a44c8bc9c9750d004e5a0890c799f304378a52
Miles <milsman2@gmail.com>
2022-12-28 01:49:19 +0000

    Remove extra step of code in return for fetch.

--- hooks/useBreweries/index.tsx
+++ hooks/useBreweries/index.tsx
@@ -3,11 +3,9 @@ import { IBrewery } from '../../src/lib/interfaces/IBreweries';
 import { GetStaticProps } from 'next';
 
 const fetchIBreweries = async (): Promise<IBrewery[]> => {
-  const res: IBrewery[] = await (
+  return await (
     await fetch('https://api.openbrewerydb.org/breweries?by_city=houston')
   ).json();
-
-  return res;
 };
 
 export const getStaticProps: GetStaticProps = async () => {

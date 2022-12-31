dc0a08671cec4ab2c5b28e208d3d79604d41b94b
Miles <milsman2@gmail.com>
2022-12-31 20:56:04 +0000

    Experiment with moving components.

--- hooks/useBreweries/index.tsx
+++ hooks/useBreweries/index.tsx
@@ -1,23 +1,10 @@
-import { useQuery, QueryClient, dehydrate } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { IBrewery } from '../../src/lib/interfaces/IBreweries';
-import { GetStaticProps } from 'next';
 
-const fetchIBreweries = async (): Promise<IBrewery[]> => {
+export const fetchIBreweries = async (): Promise<IBrewery[]> => {
   return await (
     await fetch('https://api.openbrewerydb.org/breweries?by_city=houston')
   ).json();
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery(['IBreweries'], () => fetchIBreweries());
-
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
 };
 
 export default function useBreweries() {
--- pages/BreweryPage/index.tsx
+++ pages/BreweryPage/index.tsx
@@ -1,6 +1,21 @@
 import { NextPageWithLayout } from '../_app';
 import Layout from '../../components/Layout';
 import Breweries from '../../components/Breweries';
+import { QueryClient, dehydrate } from '@tanstack/react-query';
+import { GetStaticProps } from 'next';
+import { fetchIBreweries } from '../../hooks/useBreweries';
+
+export const getStaticProps: GetStaticProps = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(['IBreweries'], () => fetchIBreweries());
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
 
 const BreweryPage: NextPageWithLayout = () => {
   return (

b2191f55ae04dfd6545758defa65299f39d0e668
Miles <milsman2@gmail.com>
2023-01-03 03:59:22 +0000

    Formatting.

--- components/SingleBrewery/index.tsx
+++ components/SingleBrewery/index.tsx
@@ -10,8 +10,11 @@ export default function SingleBrewery({ brewery }: BreweryProps) {
       <div className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-slate-300 md:text-5xl lg:text-6xl">
         {brewery.name}
       </div>
-      <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+      <p className="mb-4 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">
         {brewery.street}
+      </p>
+      <p className="mb-4 text-large font-normal text-gray-400 lg:text-large">
+        Brewery Type/Phase: {brewery.brewery_type}
       </p>
     </>
   );

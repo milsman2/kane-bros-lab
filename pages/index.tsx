d797e62a40313197ec13a333ee85359d68233093
Miles <milsman2@gmail.com>
2023-01-27 02:41:44 +0000

    Fix layout shift.

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,31 +20,29 @@ export const getStaticProps: GetStaticProps = async () => {
 
 const Home: NextPageWithLayout = () => {
   return (
-    <article className="bg-black text-slate-300 flex flex-col flex-1 items-center justify-center">
-      <section className="flex flex-col flex-1 items-center justify-center">
-        <h1 className="text-5xl font-bold">Welcome to Kane Bros. Lab</h1>
-        <Link href="/brewerypage" className="text-4xl text-slate-400 underline">
-          Breweries!
-        </Link>
-        <a
-          className="text-slate-400 underline p-4"
-          href="https://www.openbrewerydb.org/"
-        >
-          Fetched via Open Brewery DB
-        </a>
-        <a
-          className="btn btn-xs bg-slate-700 mt-24 items-center justify-center"
-          href="https://www.buymeacoffee.com/milsman2"
-        >
-          Buy me a coffee!
-        </a>
-        <h1 className="my-4 text-3xl font-extrabold tracking-tight leading-none text-slate-300 md:text-4xl lg:text-5xl">
-          Cat Fact:
-        </h1>
-        <strong>
-          <CatFact />
-        </strong>
-      </section>
+    <article className="flex flex-col flex-1 items-center justify-center bg-black text-slate-300">
+      <h1 className="text-5xl font-bold">Welcome to Kane Bros. Lab</h1>
+      <Link href="/brewerypage" className="text-4xl text-slate-400 underline">
+        Breweries!
+      </Link>
+      <a
+        className="text-slate-400 underline p-4"
+        href="https://www.openbrewerydb.org/"
+      >
+        Fetched via Open Brewery DB
+      </a>
+      <a
+        className="btn btn-xs bg-slate-700 mt-24 items-center justify-center"
+        href="https://www.buymeacoffee.com/milsman2"
+      >
+        Buy me a coffee!
+      </a>
+      <h1 className="my-4 text-3xl font-extrabold tracking-tight leading-none text-slate-300 md:text-4xl lg:text-5xl">
+        Cat Fact:
+      </h1>
+      <strong>
+        <CatFact />
+      </strong>
     </article>
   );
 };

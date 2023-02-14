8bbd09d242a8accf749e2634eb857a0c2ad6b5a4
Miles <milsman2@gmail.com>
2023-02-14 22:14:12 +0000

    image too big.

--- components/WelcomeMessage/index.tsx
+++ components/WelcomeMessage/index.tsx
@@ -1,23 +1,15 @@
-import Image from 'next/image';
-import tpl from '../../public/tpl.webp';
-
 export function WelcomeMessage() {
   return (
     <>
-      <h1 className="text-2xl lg:text-5xl md:text-4xl sm:text-3xl flex flex-row flex-wrap font-bold m-1">
+      <h1 className="text-2xl lg:text-5xl md:text-4xl sm:text-3xl flex flex-row flex-wrap font-bold m-2">
         Welcome to Kane Bros. Lab
       </h1>
-      <strong className="flex flex-row m-2">
-        Proud Incubator and Programmer of The Punters League
-      </strong>
-      <a href="https://thepuntersleague.com">
-        <Image
-          src={tpl}
-          alt="The Punters League Logo"
-          width={202}
-          height={242}
-        />
-      </a>
+      <section className="flex flex-row m-2 items-center justify-between">
+        <p className="mx-1">Proud Incubator and Programmer of</p>
+        <a href="https://thepuntersleague.com" className="underline">
+          The Punters League
+        </a>
+      </section>
     </>
   );
 }

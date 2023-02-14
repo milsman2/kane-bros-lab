d989e080d30718bb1367f9dc8f6d0f65795ca70d
Miles <milsman2@gmail.com>
2023-02-14 22:03:47 +0000

    TPL.

--- components/WelcomeMessage/index.tsx
+++ components/WelcomeMessage/index.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import tpl from '../../public/tpl.webp'
+
 export function WelcomeMessage() {
   return (
-    <h1 className="text-2xl lg:text-5xl md:text-4xl sm:text-3xl flex flex-row flex-wrap font-bold m-1">
-      Welcome to Kane Bros. Lab
-    </h1>
+    <>
+      <h1 className="text-2xl lg:text-5xl md:text-4xl sm:text-3xl flex flex-row flex-wrap font-bold m-1">
+        Welcome to Kane Bros. Lab
+      </h1>
+      <strong className="flex flex-row m-2">Proud Incubator and Programmer of The Punters League</strong>
+      <a href="https://thepuntersleague.com">
+        <Image src={tpl} alt="The Punters League Logo" width={202} height={242} />
+      </a>
+    </>
   );
 }

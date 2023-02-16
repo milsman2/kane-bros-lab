5a4d309ff6fc510925c5f58db0c13f7be3185e75
Miles <milsman2@gmail.com>
2023-02-16 03:37:48 +0000

    reduce js?

--- components/WeatherComponent/index.tsx
+++ components/WeatherComponent/index.tsx
@@ -18,25 +18,9 @@ export function WeatherComponent({
     return <strong className="loading">Loading Lab Area Weather...</strong>;
 
   return (
-    <div className="collapse">
-      <input
-        type="checkbox"
-        id="weather_dropdown"
-        aria-label="weather_dropdown"
-      />
-      <div className="collapse-title flex flex-row bg-slate-800 m-1">
-        Lab Area Weather
-        <svg
-          className="fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-        >
-          <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-        </svg>
-      </div>
-      <div className="collapse-content m-2">{children}</div>
+    <div className="flex flex-col items-center justify-center bg-slate-800 m-1">
+      <strong className="font-bold">Lab Area Weather</strong>
+      <section className="flex flex-col">{children}</section>
     </div>
   );
 }

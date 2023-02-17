75d13201a9fcb1a0a7928f0ad5c727c57df5f9b8
Miles <milsman2@gmail.com>
2023-02-17 05:40:18 +0000

    Formatting.

--- components/WelcomeMessage/index.tsx
+++ components/WelcomeMessage/index.tsx
@@ -1,12 +1,15 @@
 export function WelcomeMessage() {
   return (
     <>
-      <h1 className="text-2xl lg:text-5xl md:text-4xl sm:text-3xl flex flex-row flex-wrap font-bold m-2">
+      <h1 className="text-2xl md:text-4xl sm:text-3xl flex flex-row flex-wrap font-bold mt-1">
         Welcome to Kane Bros. Lab
       </h1>
-      <section className="flex flex-row m-2 items-center">
-        <p className="mx-1">Proud Incubator and Programmer of</p>
-        <a href="https://thepuntersleague.com" className="underline">
+      <section className="flex flex-col m-1 items-center justify-center">
+        <p className="text-lg">Proud Incubator and Programmer of</p>
+        <a
+          href="https://thepuntersleague.com"
+          className="underline font-bold text-2xl"
+        >
           The Punters League
         </a>
       </section>
